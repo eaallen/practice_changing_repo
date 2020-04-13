5e5f5cc8b2cdcafad600bc7eedf46fba82a987f7
eaallen <elijah.a.allen@gmail.com>
2020-04-13 10:08:45 -0600

    products in

---
+++ src/comps/Views/ItemCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {withFirebase} from '../Firebase'
+import * as bs from 'react-bootstrap'
+function ItemCard(props) {
+  return (
+    <div className="text-center card">
+        <div className='card-head'>
+            {props.title}
+        </div>   
+        <div className='card-body'>
+            <img className='image item_img' src={`/img/${props.id}.jpg`}/>
+            {props.price}
+        </div>   
+        
+    </div>
+  );
+}
+
+export default withFirebase(ItemCard);
--- src/comps/Views/ShowCase.js
+++ src/comps/Views/ShowCase.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import {withFirebase} from '../Firebase'
 import * as bs from 'react-bootstrap'
+import ItemCard from './ItemCard'
 function ShowCase(props) {
   return (
     <div className="">
         <bs.Row>
             <bs.Col>
-                p
+                <ItemCard title='Lorem' price='$23.33' id={1}/>
             </bs.Col>
             <bs.Col>
-            p
+                <ItemCard title='Lorem' price='$23.33'id={2}/>
             </bs.Col>
             <bs.Col>
-            p
+                <ItemCard title='Lorem' price='$23.33'id={3}/>
             </bs.Col>
         </bs.Row>       
     </div>
