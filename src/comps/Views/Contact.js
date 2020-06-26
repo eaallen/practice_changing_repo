7824bbccbe871b6472b8068010341c7cf1f6cae7
Paul Paredes <Alexparedes83@gmail.com>
2020-06-25 21:42:50 -0600

    Contact

--- src/comps/Views/Contact.js
+++ src/comps/Views/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Form,Button,Jumbotron} from 'react-bootstrap'
 
 function Contact (props){
     return(
@@ -6,6 +7,15 @@ function Contact (props){
             <h1>contact form and info here</h1>
             <br/>
             this is where {props.value} will work!
+            <Jumbotron>
+                <Form>
+                    <Form.Control type = "email" placeholder = "Email"/>
+                    <Form.Control type = "text" placeholder = "Subject"/>
+                    <Form.Control as = "textArea" placeholder = "Text"/> 
+                    <Form.Control type = "submit" value = "Submit" className = "btn btn-primary"/> 
+                </Form>
+            </Jumbotron>
+        
         </div>
     )
 }
