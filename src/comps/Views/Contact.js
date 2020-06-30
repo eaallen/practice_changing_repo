9bea8a0dc2cbf1aa034c39668235ab6599dfd884
Paul Paredes <Alexparedes83@gmail.com>
2020-06-29 19:11:07 -0600

    Contact Update

--- src/comps/Views/Contact.js
+++ src/comps/Views/Contact.js
@@ -1,21 +1,43 @@
 import React from 'react'
-import {Form,Button,Jumbotron} from 'react-bootstrap'
+import {Form,Button,Jumbotron, Row, Col} from 'react-bootstrap'
 
 function Contact (props){
     return(
         <div>
-            <h1>contact form and info here</h1>
-            <br/>
-            this is where {props.value} will work!
-            <Jumbotron>
-                <Form>
-                    <Form.Control type = "email" placeholder = "Email"/>
-                    <Form.Control type = "text" placeholder = "Subject"/>
-                    <Form.Control as = "textArea" placeholder = "Text"/> 
-                    <Form.Control type = "submit" value = "Submit" className = "btn btn-primary"/> 
-                </Form>
-            </Jumbotron>
-        
+            <h1>CONTACT FORM</h1>
+            
+                <Row>
+                    <Col lg={4}/>
+                        <Col lg={4}>
+                            <Jumbotron>
+                                <Form>
+                                    <Form.Group controlId="contactEmail">
+                                        <Form.Label>Email address</Form.Label>
+                                        <Form.Control type="email" placeholder="name@example.com" />
+                                    </Form.Group>
+                                    <Form.Group controlId="contactPurchaseID">
+                                        <Form.Label>Purchase ID Number</Form.Label>
+                                        <Form.Control type="text"/>
+                                    </Form.Group>
+                                    <Form.Group controlId="contactReason">
+                                        <Form.Label>Reason for Email</Form.Label>
+                                        <Form.Control as="select">
+                                        <option>Need money back</option>
+                                        <option>I love it!</option>
+                                        <option>IDK</option>
+                                        <option>Just to say hi</option>
+                                        <option>Other..</option>
+                                        </Form.Control>
+                                    </Form.Group>
+                                    <Form.Group controlId="contactMsg">
+                                        <Form.Label>Message</Form.Label>
+                                        <Form.Control as="textarea" rows="4" placeholder="Text here"/>
+                                    </Form.Group>
+                                </Form>
+                            </Jumbotron>
+                        </Col>
+                </Row>
+            
         </div>
     )
 }
