746d8e9381d9d29fd25bfbbbb2ac44175ffe5ffe
eaallen <elijah.a.allen@gmail.com>
2020-07-01 20:28:29 -0600

    bug fixed

--- src/comps/Firebase/Firebase.js
+++ src/comps/Firebase/Firebase.js
@@ -112,6 +112,7 @@ export const AppContext = React.createContext()
           })
         }
         doCreateOneRecord = async(_collection, obj) =>{
+          obj.product_date = firebase.firestore.Timestamp.fromDate(new Date())
           let success = false
           await this.db.collection(_collection).add(obj).then(()=>{
             console.log("it is deleted")
