abc7d50eff92a74971c9c055ed6e53f738fd1d78
eaallen <elijah.a.allen@gmail.com>
2020-07-13 00:39:38 -0600

    update multi

--- src/comps/Forms/Admin/Cart/CartCreateFrom.js
+++ src/comps/Forms/Admin/Cart/CartCreateFrom.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import { Form, Row, Col, Button, Collapse } from 'react-bootstrap'
 import {withFirebase} from '../../../Firebase'
-import AddressCreateFrom from '../Address/AddressCreateFrom'
-import Toggle from '../../../Tools/Toggle'
 import produce from 'immer'
+import MultiSelect from '../../../Tools/MultiSelect'
 class CartrCreateForm extends React.Component{
     constructor(props){
         super(props)
@@ -16,27 +15,40 @@ class CartrCreateForm extends React.Component{
         }
     }
 
+    componentDidMount = async() =>{
+        const arr_customers = await this.props.context.doQueryAll("customer")
+        const arr_products = await this.props.context.doQueryAll("product")
+        this.setState({
+            customers:arr_customers,
+            products:arr_products,
+        })
+    }
+
     handleChange(e){
         console.log(e.target)
         this.setState({[e.target.name]:e.target.value})
     }
 
-    removeAddress = (idx) =>{
-        console.log("idx----->",idx)
-        const arr = this.state.addresses.filter(comp=> this.state.addresses.indexOf(comp)!==idx)
-        console.log(arr)
-        this.setState(state=> produce(state, draft=>{
-            draft.addresses = arr
-        })) 
-    }
     submit = async(e) =>{
         e.preventDefault()
         const data = this.state
         delete data.addShippingAddress
         delete data.addresses
-        this.props.context.doCreateOneRecord("customer",data)
+        this.props.context.doCreateOneRecord("cart",data)
     }
     render(){
+        if(!this.state.products || !this.state.customers){
+            return(<div>loading...</div>)
+        }
+        const select_prod = []
+        for(const product of this.state.products){
+            //[false, product.product_name]
+            select_prod.push({
+                id:product.id,
+                selected:false,
+                name: product.product_name
+            })
+        }
         return(
             <div>
                 <Row>
@@ -44,36 +56,32 @@ class CartrCreateForm extends React.Component{
                     <Col lg={8}>
                         <Form onSubmit={e=>this.submit(e)}>
                             <Form.Group controlId="customerEmail">
-                                <Form.Label>Email:</Form.Label>
-                                <Form.Control type="email"
-                                    placeholder="xample@mail.com"
-                                    required
-                                    name="customer_email"
-                                    value={this.state.customer_email}
+                                <Form.Label>Select Customer</Form.Label>
+                                <Form.Control 
+                                    as="select"
+                                    name="customer_size"
+                                    value={this.state.customer_size}
                                     onChange={e => this.handleChange(e)}
-                            />
+                                >
+                                    {this.state.customers.map(item=>{
+                                        return(
+                                            <option value={item.id}>{item.customer_email}</option>
+                                        )
+                                    })}
+                                </Form.Control>
                             </Form.Group>
 
 
                             <Row>
-                                <Col lg={2}>
+                                <Col lg={4}>
                                     <Form.Group controlId="customerSize">
-                                        <Form.Label>Select Size</Form.Label>
-                                            <Form.Control 
-                                                as="select"
-                                                name="customer_size"
-                                                value={this.state.customer_size}
-                                                onChange={e => this.handleChange(e)}
-                                            >
-                                                <option value="xxs">XXS</option>
-                                                <option value="xs">XS</option>
-                                                <option value="s">S</option>
-                                                <option value="m">M</option>
-                                                <option value="l">L</option>
-                                                <option value="xl">XL</option>
-                                            </Form.Control>
+                                        <Form.Label>Select Products</Form.Label>
+                                        <MultiSelect 
+                                            opt={select_prod} 
+                                            change_avaliable_sizes={this.change_avaliable_sizes}
+                                            cart
+                                        />                                    
                                     </Form.Group>
-
                                 </Col>
                             </Row>
                             <Button variant="primary" type="submit">
--- src/comps/Tools/MultiSelect.js
+++ src/comps/Tools/MultiSelect.js
@@ -14,12 +14,39 @@ export default class MultiSelect extends React.Component{
         this.props.change_avaliable_sizes(val,name)
     }
     render(){
+        if(this.props.cart){
+            return(
+                <div>
+                    {this.props.opt.map(item=>{
+                        return(
+                            <div key={item.id}>
+                                <input 
+                                    type="checkbox" 
+                                    name={item.name}
+                                    id={item.id} 
+                                    checked={item.selected} 
+                                    onChange={e=> this.handleChange(e)}
+                                /> {item.name} 
+                                 
+                            </div>
+                        )
+                    })}
+                </div>
+            )
+    
+        }
         return(
             <div>
                 {Object.entries(this.props.opt).map(item=>{
                     return(
                         <div key={item}>
-                            <input type="checkbox" name={item[0]} checked={item[1]} onChange={e=> this.handleChange(e)}/> {item[0].toUpperCase()}
+                            <input 
+                                type="checkbox" 
+                                name={item[0]} 
+                                checked={item[1]} 
+                                onChange={e=> this.handleChange(e)}
+                            /> 
+                            {item[0].toUpperCase()}
                         </div>
                     )
                 })}
